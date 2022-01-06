96acff340c3ef6f763329a4c0527c10e61951398
kodandareddy <kodandareddy21@gmail.com>
2022-01-06 18:52:36 +0530

    monourl removed

--- Server/index.js
+++ Server/index.js
@@ -1,19 +1,24 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors'
-import postRoutes from './Routes/posts.js'
-const app=express();
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./Routes/posts.js";
+const app = express();
 
-app.use(bodyParser.json({limit:"30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 
-const CONNECTION_URL='mongodb+srv://kodanda:Kodanda@123@cluster0.9duqt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT=process.env.PORT || 5000
+const CONNECTION_URL = process.env.monogoUrl;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
